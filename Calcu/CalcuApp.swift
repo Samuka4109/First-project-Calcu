40e808846a07fbb862a10fb2a2b9228b2f360f33
samuel.parnaiba <samuel.parnaiba@icloud.com>
2024-01-07 10:12:24 -0300

    Initial Commit

---
+++ Calcu/CalcuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcuApp.swift
+//  Calcu
+//
+//  Created by SAMUEL BARROS on 05/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CalcuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
